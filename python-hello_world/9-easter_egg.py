49da61e2ef0a21431b3f5d9c1c4964550de2bd47
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-14 14:37:49 +0100

    Adding a Python script which print a text

---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+str1 = "The Zen of Python, by Tim Peters\n"
+str2 = "Beautiful is better than ugly."
+str3 = "Explicit is better than implicit."
+str4 = "Simple is better than complex."
+str5 = "Complex is better than complicated."
+str6 = "Flat is better than nested."
+str7 = "Sparse is better than dense."
+str8 = "Readability counts."
+str9 = "Special cases aren't special enough to break the rules."
+str10 = "Although practicality beats purity."
+str11 = "Errors should never pass silently."
+str12 = "Unless explicitly silenced."
+str13 = "In the face of ambiguity, refuse the temptation to guess."
+str14 = "There should be one-- and preferably only one --obvious way to do it."
+str15 = "Although that way may not be obvious at first unless you're Dutch."
+str16 = "Now is better than never."
+str17 = "Although never is often better than *right* now."
+str18 = "If the implementation is hard to explain, it's a bad idea."
+str19 = "If the implementation is easy to explain, it may be a good idea."
+str20 = "Namespaces are one honking great idea -- let's do more of those!"
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+print(str6)
+print(str7)
+print(str8)
+print(str9)
+print(str10)
+print(str11)
+print(str12)
+print(str13)
+print(str14)
+print(str15)
+print(str16)
+print(str17)
+print(str18)
+print(str19)
+print(str20)

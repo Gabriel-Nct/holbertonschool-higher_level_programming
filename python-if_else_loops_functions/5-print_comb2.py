6f56b50d382cd65bf0bfce297e5e1e93fb420326
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-15 15:47:04 +0100

    Adding a second print for the last number

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    print("{:02d}".format(number), end=", " if number < 99 else "\n")
-    
+for number in range(0, 99):
+    print("{:02d}, ".format(number), end="")
+print("{:02d}".format(99))

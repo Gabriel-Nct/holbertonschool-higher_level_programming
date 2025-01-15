657b32539523734ed4d276077774cf5f83b59401
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-15 17:11:48 +0100

    Removing the special case

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,6 @@
 def islower(c):
 
     if ord(c) > 96 and ord(c) < 123:
-        return True
-    elif ord(c) == c:
-        return True
+        return (True)
     else:
-        return False
+        return (False)

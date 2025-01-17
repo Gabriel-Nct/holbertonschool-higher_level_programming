d1157953026aa7206bcc5f0a92e8f46a9e625a6e
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-17 09:41:02 +0100

    Fixing the pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for number in range(1,101):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz", end=" ")
         elif number % 3 == 0:
             print("Fizz", end=" ")
             continue
         elif number % 5 == 0:
-            print("Buzz",end=" ")
+            print("Buzz", end=" ")
             continue
         else:
             print(number, end=" ")
-

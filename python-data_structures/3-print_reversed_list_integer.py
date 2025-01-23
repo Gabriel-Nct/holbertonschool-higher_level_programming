f07514bce8b1d1d5b3331101ebb7bd9ae495e098
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-23 12:58:08 +0100

    Adding a specifier to print

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for index_number in my_list[::-1]:
-        print("{}".format(index_number))
+        print("{:d}".format(index_number))

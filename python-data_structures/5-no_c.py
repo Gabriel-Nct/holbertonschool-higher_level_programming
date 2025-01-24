6a6606cd97fc87c658506b222e7e8502f1aa0d26
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-24 10:27:01 +0100

    Readjusting the code

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = " "
+    newstring = ""
     for letter in my_string:
-        if letter != 'c' and letter != 'C':
-            new_string += letter
-    return (new_string)
+        if letter != "c" and letter != "C":
+            newstring += letter
+    return newstring

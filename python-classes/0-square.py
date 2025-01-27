8b27c89612f1eec01d95c79ff48ecafeaecb0c55
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-27 17:42:10 +0100

    Adding documentation to the file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+"""Module 0-square: Definition of the Square class.
+
+This module contains an empty class called Square, which defines a square.
+The class serves as a foundation for further exercises or future development.
+
+Attributes:
+    None defined in this module.
+
+Todo:
+    * Add functionality to the Square class in future tasks.
+"""
+
+
 class Square:
+    """An empty class that defines a square.
+
+    This class currently has no attributes or methods but serves as a starting
+    point for the exercise requirements.
+    """
     pass

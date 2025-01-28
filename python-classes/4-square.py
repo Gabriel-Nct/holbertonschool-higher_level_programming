e2021a6f6fe8922811d548030379a594db7028c8
Gabriel-Nct <bescondgabriel@gmail.com>
2025-01-28 13:30:18 +0100

    Adding a setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Module 3-square: Definition of the Square class.
+
+This module class Square that defines a square by: (based on 2-square.py)
+
+"""
+
+
+class Square:
+    """Square with size
+
+    """
+    def __init__(self, size=0):
+        """
+        give the size
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        give the area of the square
+        """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """
+        give the size of the square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        """
+        set the size
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size

05763256925fd765ac97a4b1b48470acfb9637b7
Gabriel-Nct <bescondgabriel@gmail.com>
2025-02-05 14:23:39 +0100

    Adding area with a exception message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Geometry module: Definition of the base geometry class.
+This module is class BaseGeometry that defines a Base Geometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class called BaseGeometry.
+    """
+    def area(self):
+        """Summary
+        """
+        raise Exception("area() is not implemented")

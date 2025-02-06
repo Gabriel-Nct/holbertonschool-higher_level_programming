21f86acc71d105469a8f2a04a42e7807a40f6c6b
Gabriel-Nct <bescondgabriel@gmail.com>
2025-02-06 17:37:40 +0100

    Changeing the print with .format

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,9 +10,10 @@ class BaseGeometry:
     A class called BaseGeometry.
     """
     def area(self):
-        """Raises an exception indicating that the area method is not implemented."""
+        """Raises an exception indicating
+        that the area method is not implemented."""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Validates that value is a positive integer.
 
@@ -25,6 +26,8 @@ class BaseGeometry:
             ValueError: If value is less than or equal to 0.
         """
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
+        else:
+            return True

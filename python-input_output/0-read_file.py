11b5a098fea76ee64b9ff3f028c38e84048d87a7
Gabriel-Nct <bescondgabriel@gmail.com>
2025-02-10 13:57:48 +0100

    Adding a function that reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Function which open and reads a file.txr
+    Args:
+        filename (str): Text to open. Defaults to "".
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)

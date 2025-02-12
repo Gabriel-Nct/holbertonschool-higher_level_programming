fa40cad2aca4b513fcdfb5426e3d4fca5af07769
Gabriel-Nct <bescondgabriel@gmail.com>
2025-02-12 17:58:54 +0100

    Changeing commentaries in the second docstring

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -5,11 +5,14 @@ function that returns the JSON representation of an object (string)
 import json
 
 
-
 def from_json_string(my_str):
-    """_summary_
+    """
+    Convert JSON in a string
 
-    Args:
-    my_obj (_type_): _description_
+     Args:
+        my_str (str): string to convert
+
+    Returns:
+        JSON
     """
     return json.loads(my_str)
